400f77717f684ec3a44bd509a8abcebf86e1ae7c
seryl lounis <seryllounis@outlook.fr>
2021-04-12 23:15:12 +0200

    Back-end finish

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,8 +1,8 @@
 const UserModel = require("../models/user.model");
 const fs = require("fs");
 const { promisify } = require("util");
-const { uploadErrors } = require("../utils/errors.utils");
 const pipeline = promisify(require("stream").pipeline);
+const { uploadErrors } = require("../utils/errors.utils");
 
 module.exports.uploadProfil = async (req, res) => {
   try {
